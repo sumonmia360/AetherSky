2eaef9e87c6eb38936c1e9565db0832fbc5ddedc
sumonmia360 <sumonmia4526@gmail.com>
2025-09-22 22:36:22 +0600

    deployment done

--- src/app/index/page.tsx
+++ src/app/index/page.tsx
@@ -100,7 +100,7 @@ export default function Index() {
     queryKey: ["repoData"],
     queryFn: async () => {
       const { data } = await axios.get(
-        `http://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
+        `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
       );
       return data;
     },
